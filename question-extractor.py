192489b710290905a465da97173436c2171cd54c
Joshua Cohen <65085812+joshuajohncohen@users.noreply.github.com>
2025-06-24 21:10:38 -0400

    Add more to question extractor: make it crop the pages to questions, and save them with a json about them

--- question-extractor.py
+++ question-extractor.py
@@ -1,4 +1,4 @@
-from typing import Dict, List, Set
+from typing import Dict, List, Set, Tuple
 
 from google import genai
 from google.genai import types
@@ -7,6 +7,8 @@ import os
 from os import getenv
 from dotenv import load_dotenv
 load_dotenv()
+
+import shutil
 
 from pdf2image import convert_from_path
 from PIL import Image
@@ -19,7 +21,7 @@ import json
 
 client = genai.Client(api_key = getenv("API_KEY"))
 
-def pdf_get_pages(pdf_path: str):
+def pdf_get_pages(pdf_path: str) -> List[Image.Image]:
     return convert_from_path(pdf_path)
 
 def pdf_save_pages(pdf_path, test_folder: str) -> List[str]:
@@ -55,7 +57,7 @@ def get_tags(test_pdf: str) -> Set[str]:
         tags = response.text.split(",")
     return set(tags)
 
-def get_questions(pages_folder, test_folder: str):
+def get_question_crops(pages_folder, test_folder: str):
     pages_paths = list(os.listdir(pages_folder))
 
     page_question_data = {}
@@ -68,7 +70,7 @@ def get_questions(pages_folder, test_folder: str):
         with open(page_path, "rb") as file:
             prompt = \
             """You are part of an app for drilling Regents questions indexed from past tests. Your task is to help extract the questions from the attached scan JPEG of a page from a test book. Answer in the following format:"""+\
-            """[{"question_number":42,"bounds":((62,254),(100,500)),"question_type":"free_response",tags:["tag1","tag2"]},...]"""+\
+            """[{"question_number":42,"bounds":(62,254,100,500),"question_type":"free_response",tags:["tag1","tag2"]},...]"""+\
             """Bounds is the coordinates in the pixel grid of the top left and bottom right corners of the bounding box which encloses the question and its answers, for cropping. Question type can either be "free_response" for the later free response questions, or "multiple_choice" for the standard multiple choice questions. The tags are these: """ + ", ".join(tags) +\
             """Only answer with this information in this structured format and no extraneous or additional information."""
             response = client.models.generate_content(
@@ -97,10 +99,31 @@ def get_questions(pages_folder, test_folder: str):
         with open(f"{test_folder}/question_crop.json", "w") as file:
             file.write(json.dumps(questions))
 
-# TODO: Add crop_questions() to take each question and get the page and crop the bounding box and save each question to folder 'questions' in its own folder just named the question number, with the image + the cropped just of the question itself and just of the answer choices (if its a multiple choice), also if its free response check if it has sub-questions and save those, and save some jsons for these to store all the info
+def crop_questions(test_folder: str):
+    with open(f"{test_folder}/question_crop.json", "r") as file:
+        question_crops = json.loads(file.read())
 
-def analyze_test(test_folder: str) -> Dict[int, Dict]:
-    # work in progress
+    if os.path.exists(f"{test_folder}/questions"):
+        shutil.rmtree(f"{test_folder}/questions")
+    os.mkdir(f"{test_folder}/questions")
+
+    for question_crop in question_crops:
+        page_original = Image.open(f"{test_folder}/pages/page_{question_crop["page_number"]}.jpeg")
+
+        cropped_page = page_original.crop(question_crop["bounds"])
+
+        os.mkdir(f"{test_folder}/questions/{question_crop["page_number"]}")
+
+        cropped_page.save(f"{test_folder}/questions/{question_crop["page_number"]}/question.jpeg")
+
+        question_metadata = {"question_number": question_crop["question_number"], "question_type": question_crop["question_type"], "tags": question_crop["tags"], "page_number": question_crop["page_number"]}
+
+        with open(f"{test_folder}/questions/{question_crop["page_number"]}/metadata.json", "w") as file:
+            file.write(json.dumps(question_metadata))
+
+def analyze_test(test_folder: str):
     pages_paths = pdf_save_pages(f"{test_folder}/test.pdf", f"{test_folder}/pages")
 
-    tags = get_tags(f"{test_folder}/test.pdf")
+    get_question_crops(f"{test_folder}/pages", test_folder)
+
+    crop_questions(test_folder)
